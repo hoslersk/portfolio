01e3aa2c6b8264c6159f62b283578e0c4eb0cb90
Schyler Hosler <schylerhosler@gmail.com>
2017-03-03 21:53:13 -0500

    Initial set-up of temp landing page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import background from '../public/qinghill.jpg'
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Schyler Hosler</h2>
-        </div>
-        <p className="App-intro">
-          UNDER CONSTRUCTION
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<div className="App-header">
+					<img src={background} className="background" alt="background" />
+					<h1 className="schyler2">
+						Schyler Hosler is ...
+					</h1>
+					<h1 className="schyler1">
+						Schyler Hosler is ...
+					</h1>
+				</div>
+				<h2 className="headline2">
+					UNDER CONSTRUCTION
+				</h2>
+				<h2 className="headline1">
+					UNDER CONSTRUCTION
+				</h2>
+			</div>
+		);
+	}
 }
 
 export default App;
