f46eaacdb8e5d17201cbc39a278cb2d199c47354
Schyler Hosler <schylerhosler@gmail.com>
2017-03-04 22:55:46 -0500

    Updates contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,6 +5,9 @@ export default class Contact extends Component {
 		return (
 			<div className="page-contents">
 				<h1 className="page-title">Contact Schyler</h1>
+				<div className="about-content">
+					<img src="https://media.giphy.com/media/dcLwBDsjZHjl6/giphy.gif" />
+				</div>
 			</div>
 		)
 	}
