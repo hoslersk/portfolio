ee82cf60e529688623e81c2ab5fa88c1f7308818
Schyler Hosler <schylerhosler@gmail.com>
2017-03-06 23:06:32 -0500

    Testing with SVG and styling changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import DevIcon from '../components/devicon'
+
 import me1 from '../../public/me1.jpg'
 import me2 from '../../public/me2.jpg'
 import me3 from '../../public/me3.jpg'
@@ -9,14 +11,29 @@ export default class About extends Component {
 		return (
 			<div className="page-contents">
 				<h1 className="page-title">About Schyler</h1>
+				{/* <div className="icon-container">
+					<DevIcon value="github" />
+					<DevIcon value="javascript" />
+					<DevIcon value="html5" />
+					<DevIcon value="heroku" />
+					<DevIcon value="git" />
+					<DevIcon value="css3" />
+					<DevIcon value="sass" />
+					<DevIcon value="postgres" />
+					<DevIcon value="mongodb" />
+					<DevIcon value="atom" />
+					<DevIcon value="jquery" />
+				</div> */}
 				<div className="about-content">
 					Interests:
 					<ul>
 						<li>Web Development</li>
 						<li>Peanut Butter</li>
 						<li>PlayStation</li>
+						<li>Digimon</li>
 						<li>JRPGs</li>
 						<li>Horror</li>
+						<li>Visual Novels</li>
 					</ul>
 					Experience:
 					<ul>
@@ -37,8 +54,7 @@ export default class About extends Component {
 				</div>
 				<img src={me3} className="me-pic" />
 				<img src={me2} className="me-pic" />
-				<i className="devicon-github-plain" />
-				<i className="devicon-git-plain" />
+
 			</div>
 		)
 	}
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,8 +5,31 @@ export default class Contact extends Component {
 		return (
 			<div className="page-contents">
 				<h1 className="page-title">Contact Schyler</h1>
-				<div className="about-content">
-					<img src="https://media.giphy.com/media/dcLwBDsjZHjl6/giphy.gif" />
+				<div className="contact-content">
+					<img
+						src="https://media.giphy.com/media/dcLwBDsjZHjl6/giphy.gif"
+						className="ppg-phone"
+					/>
+					<a
+						href="https://github.com/hoslersk"
+						target="_blank"
+						className="external-link"
+					>Github</a>
+					<a
+						href="https://www.linkedin.com/in/schyler-hosler-a282aa27"
+						target="_blank"
+						className="external-link"
+					>LinkedIn</a>
+					<a
+						href="https://my.playstation.com/digivotary"
+						target="_blank"
+						className="external-link"
+					>PlayStation</a>
+					<a
+						href="https://twitter.com/schylerhosler"
+						target="_blank"
+						className="external-link"
+					>Twitter</a>
 				</div>
 			</div>
 		)
