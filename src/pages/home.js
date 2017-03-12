836f1414c3c83241a61c43fef0569ff67b27f6ba
Schyler Hosler <schylerhosler@gmail.com>
2017-03-12 16:03:41 -0400

    Updates landing page (temporary update)

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,13 +4,20 @@ import TempBG from '../components/tempbg'
 import Schyler from '../components/schyler'
 import Construction from '../components/construction'
 
+import arrow from '../../public/arrow.png'
+import circled from '../../public/circled.png'
+import stillOpen from '../../public/stillopen.png'
+
 export default class Home extends Component {
 	render() {
 		return (
 			<div>
-			 <TempBG />
-			 <Schyler />
-			 <Construction />
+				<TempBG />
+				<Schyler />
+				<Construction />
+				<img src={arrow} className="arrow" />
+				<img src={circled} className="circled" />
+				<img src={stillOpen} className="stillOpen" />
 			</div>
 		)
 	}
